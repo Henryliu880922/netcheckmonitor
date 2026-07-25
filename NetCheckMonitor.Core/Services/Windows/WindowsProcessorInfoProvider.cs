481096114c1d857c7f0258f4c69a3f5352e825d5
Henryliu880922 <34577354+Henryliu880922@users.noreply.github.com>
2026-07-26 02:37:15 +0800

    Add Windows processor information provider

--- NetCheckMonitor.Core/Services/Windows/WindowsProcessorInfoProvider.cs
+++ NetCheckMonitor.Core/Services/Windows/WindowsProcessorInfoProvider.cs
@@ -1,11 +1,38 @@
 namespace NetCheckMonitor.Core.Services.Windows;
 
+using System.Management;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
 using NetCheckMonitor.Core.Models.SystemInfo;
 
+[SupportedOSPlatform("windows")]
 internal static class WindowsProcessorInfoProvider
 {
     public static ProcessorInfo GetProcessorInfo()
     {
-        throw new NotImplementedException();
+        if (!OperatingSystem.IsWindows())
+        {
+            throw new PlatformNotSupportedException();
+        }
+
+        using ManagementObjectSearcher searcher =
+            new("SELECT Name, Manufacturer, NumberOfCores, " + "NumberOfLogicalProcessors, MaxClockSpeed " + "FROM Win32_Processor");
+
+        foreach (ManagementObject processor in searcher.Get())
+        {
+            return new ProcessorInfo
+            {
+                Model = processor["Name"]?.ToString() ?? string.Empty,
+                Manufacturer = processor["Manufacturer"]?.ToString() ?? string.Empty,
+                Architecture = RuntimeInformation.ProcessArchitecture.ToString(),
+                PhysicalCoreCount = Convert.ToInt32(processor["NumberOfCores"] ?? 0),
+                LogicalCoreCount = Convert.ToInt32(processor["NumberOfLogicalProcessors"] ?? 0),
+                MaxClockSpeedMhz = Convert.ToDouble(processor["MaxClockSpeed"] ?? 0),
+            };
+        }
+        return new ProcessorInfo
+        {
+            Architecture = RuntimeInformation.ProcessArchitecture.ToString(),
+        };
     }
 }
