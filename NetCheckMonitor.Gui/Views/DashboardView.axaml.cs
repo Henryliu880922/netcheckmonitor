611d440c2aa6a5b96621557a610392c185f0754a
Henryliu880922 <34577354+Henryliu880922@users.noreply.github.com>
2026-07-25 22:53:06 +0800

    Create dashboard view

---
+++ NetCheckMonitor.Gui/Views/DashboardView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace NetCheckMonitor.Gui.Views;
+
+public partial class DashboardView : UserControl
+{
+    public DashboardView()
+    {
+        InitializeComponent();
+    }
+}
