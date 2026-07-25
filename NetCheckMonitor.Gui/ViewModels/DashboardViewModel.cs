18e6b6c56a404de2752f08396689a727ecf2894a
Henryliu880922 <34577354+Henryliu880922@users.noreply.github.com>
2026-07-25 23:33:24 +0800

    Bind monitor settings

--- NetCheckMonitor.Gui/ViewModels/DashboardViewModel.cs
+++ NetCheckMonitor.Gui/ViewModels/DashboardViewModel.cs
@@ -1,6 +1,15 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+
 namespace NetCheckMonitor.Gui.ViewModels;
 
-public class DashboardViewModel
+public partial class DashboardViewModel : ObservableObject
 {
-    
+    [ObservableProperty]
+    private string host = "8.8.8.8";
+
+    [ObservableProperty]
+    private int interval = 1000;
+
+    [ObservableProperty]
+    private int timeout = 3000;
 }
