ea690794f84644e1dc0ab45cb415698930f5a6e7
Henryliu880922 <34577354+Henryliu880922@users.noreply.github.com>
2026-07-26 20:57:24 +0800

    Complete WindowsSystemInfoService provider integration

--- NetCheckMonitor.Core/Services/Windows/WindowsSystemInfoService.cs
+++ NetCheckMonitor.Core/Services/Windows/WindowsSystemInfoService.cs
@@ -2,6 +2,9 @@ namespace NetCheckMonitor.Core.Services.Windows;
 
 using NetCheckMonitor.Core.Models.SystemInfo;
 using NetCheckMonitor.Core.Services.Interfaces;
+using System.Runtime.Versioning;
+
+[SupportedOSPlatform("windows")]
 
 public sealed class WindowsSystemInfoService : ISystemInfoService
 {
@@ -12,6 +15,15 @@ public sealed class WindowsSystemInfoService : ISystemInfoService
         var systemInfo = new SystemInfo
         {
             Computer = WindowsComputerInfoProvider.GetComputerInfo(),
+            OperatingSystem = WindowsOperatingSystemInfoProvider.GetOperatingSystemInfo(),
+            Processor = WindowsProcessorInfoProvider.GetProcessorInfo(),
+            Memory = WindowsMemoryInfoProvider.GetMemoryInfo(),
+            StorageDevices = WindowsStorageInfoProvider.GetStorageInfo(),
+            Displays = WindowsDisplayInfoProvider.GetDisplays(),
+            NetworkAdapters = new WindowsNetworkInfoProvider().GetNetworkAdapters(),
+            Firmware = WindowsFirmwareInfoProvider.GetFirmwareInfo(),
+            Security = WindowsSecurityInfoProvider.GetSecurityInfo(),
+            Battery = new WindowsBatteryInfoProvider().GetBatteryInfo(),
         };
 
         return Task.FromResult(systemInfo);
