3811eabe1068b702ce6601e4992263ebcf595f24
Henryliu880922 <34577354+Henryliu880922@users.noreply.github.com>
2026-07-27 13:43:00 +0800

    feat(gui): display system information

--- NetCheckMonitor.Gui/ViewModels/DashboardViewModel.cs
+++ NetCheckMonitor.Gui/ViewModels/DashboardViewModel.cs
@@ -1,12 +1,18 @@
 using System;
 using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
 using NetCheckMonitor.Core.Models;
+using NetCheckMonitor.Core.Models.SystemInfo;
+using NetCheckMonitor.Core.Services;
+using NetCheckMonitor.Core.Services.Interfaces;
 
 namespace NetCheckMonitor.Gui.ViewModels;
 
 public partial class DashboardViewModel : ObservableObject
 {
+    private readonly ISystemInfoService systemInfoService;
+
     [ObservableProperty]
     private string host = "8.8.8.8";
 
@@ -16,10 +22,36 @@ public partial class DashboardViewModel : ObservableObject
     [ObservableProperty]
     private int timeout = 3000;
 
+    private SystemInfo? systemInformation;
+
+    public SystemInfo? SystemInformation
+    {
+        get => systemInformation;
+        private set => SetProperty(ref systemInformation, value);
+    }
+
+    private string? systemInfoError;
+
+    public string? SystemInfoError
+    {
+        get => systemInfoError;
+        private set => SetProperty(ref systemInfoError, value);
+    }
+
+    private bool isLoadingSystemInfo;
+
+    public bool IsLoadingSystemInfo
+    {
+        get => isLoadingSystemInfo;
+        private set => SetProperty(ref isLoadingSystemInfo, value);
+    }
+
     public ObservableCollection<MonitorResult> Results { get; } = new();
 
     public DashboardViewModel()
     {
+        systemInfoService = SystemInfoServiceFactory.Create();
+
         Results.Add(new MonitorResult
         {
             Timestamp = DateTime.Now,
@@ -27,5 +59,27 @@ public partial class DashboardViewModel : ObservableObject
             Status = "Success",
             RoundTripTime = 18
         });
+
+        _ = LoadSystemInfoAsync();
+    }
+
+    private async Task LoadSystemInfoAsync()
+    {
+        try
+        {
+            IsLoadingSystemInfo = true;
+            SystemInfoError = null;
+
+            SystemInformation =
+                await systemInfoService.GetSystemInfoAsync();
+        }
+        catch (Exception exception)
+        {
+            SystemInfoError = exception.Message;
+        }
+        finally
+        {
+            IsLoadingSystemInfo = false;
+        }
     }
 }
